8403ca8dc759df27db9c955bd4c90a9bd5c290c5
NatashaRichter <nlr9t@virginia.edu>
2015-07-15 13:58:38 -0400

    File uploaded on behalf of the Open Science Framework

---
+++ mysite.js
@@ -0,0 +1,23 @@
+
+var age = 26;
+var maxAge = 120;
+var yearsLeft = maxAge-age;
+var amountPerDay = 5;
+var daysPerYear = 365;
+var amountPerLife = amountPerDay*daysPerYear*yearsLeft;
+var answer = "You will need" + amountPerLife + "to last you until your old age of" + maxAge +".";
+
+alert(answer);
+
+if {amountPerLife > 40,000
+	alert("Wow! That's a lot!");
+};
+
+else if {amountPerLife < 40,000
+	alert("You seem pretty reasonable!"")
+};
+
+
+function calculate(){
+	
+}
